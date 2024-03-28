1a5c523b7b5df5393e22bc3686b4e866c0679cdb
000web <101223443+000web@users.noreply.github.com>
2024-03-28 15:01:54 -0600

    Beta 1.0
    Beta 1.0

---
+++ email_template_script.js
@@ -0,0 +1,20 @@
+const logo1 = document.querySelector("#lx1");
+logo1.addEventListener("click", ()=>{
+    window.location.href = "https://angelicaarteagajoyeria.com/inicio"
+});
+const logo2 = document.querySelector("#lx2");
+logo2.addEventListener("click", ()=>{
+    window.location.href = "https://angelicaarteagajoyeria.com/inicio"
+});
+const logo3 = document.querySelector("#lx3");
+logo3.addEventListener("click", ()=>{
+    window.location.href = "https://angelicaarteagajoyeria.com/inicio"
+});
+const buy1 = document.querySelector("#buy1");
+buy1.addEventListener("click", ()=>{
+    window.location.href = "https://angelicaarteagajoyeria.com/tienda/mayoreo/en-stock"
+});
+const fred1 = document.querySelector("#fx1");
+fred1.addEventListener("click", ()=>{
+    window.location.href = "https://api.whatsapp.com/send?phone=+525548427028&text=Hola Fredy! Me interesa tu trabajo en Desarrollo Web."
+});
